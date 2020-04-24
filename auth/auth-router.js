584a2fdd83aa9dd4a72a4c82a5614b6f3bd665fb
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-24 11:08:49 -0400

    implement register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 
+Users = require('../models/users-model.js');
+
 router.post('/register', (req, res) => {
-  // implement registration
+  let user = req.body;
+  const rounds = process.env.HASH_ROUNDS || 14;
+  user.password = hash;
+
+  Users.add(user)
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: err.message })
+    })
+
 });
 
 router.post('/login', (req, res) => {
